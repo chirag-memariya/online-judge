57b188c1d4c135524f93a29345a0540d3fba02fa
chirag-memariya <memariyachirag@gmail.com>
2024-08-23 00:09:39 +0530

    feat: add C++ compiler integration
    - integrate C++ code compiler in the backend
    - handle code execution requests via `/run` endpoint

---
+++ compiler/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { generateFile } = require('./generateFile.js');
+const { executeCpp } = require('./execureCpp.js');
+
+const app = express();
+ 
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+app.get("/",(req,res)=>{
+    res.send("Home");
+});
+
+app.post("/run",async (req,res)=>{
+    const {language='cpp',code} = req.body;
+    
+    if(code==undefined){
+        return res.status(400).json({ error: "code is required."});
+    }
+
+    try {
+        const filePath = generateFile(language,code);
+        const output = await executeCpp(filePath);
+        res.json({output});
+    } catch (error) {
+        return res.status(500).json({ error: error.message});
+    }
+
+    res.json( {language,code} );
+});
+
+app.listen(8080,()=>{
+    console.log("server is listening on post 8080");
+});
