751639bc2ba476d436f8db1d84dc16830972aa5f
chirag-memariya <memariyachirag@gmail.com>
2024-09-26 20:03:57 +0530

    feat: add company name

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -3,30 +3,19 @@ import React from 'react'
 const Footer = () => {
   return (
 
-<footer className="bg-white shadow dark:bg-gray-900">
-    <div className="w-full max-w-screen-xl mx-auto p-4 md:py-4">
-        <div className="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                <span className=" title self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-            </a>
-            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
-                </li>
-                <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" className="hover:underline">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
+<footer className="bg-white shadow dark:bg-gray-900 relative">
+  <div className="w-full max-w-screen-xl p-4 md:py-4 flex items-center justify-between">
+    <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+      <span className="title self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CodeThrone</span>
+    </a>
+    <p className="absolute left-0 right-0 text-center text-indigo-300 text-sm mx-auto">
+      © 2024 CodeThrone. All rights reserved.
+    </p>
+  </div>
 </footer>
+
+
 
   )
 }
--- frontend/src/pages/ProblemList.jsx
+++ frontend/src/pages/ProblemList.jsx
@@ -5,7 +5,7 @@ const ProblemList = () => {
   return (
     <>
       <h3 className="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight p-5 shadow">
-       Welcome to <span className="title text-indigo-600"> Online Judge</span>
+       Welcome to <span className="title text-indigo-600">CodeThrone!</span>
       </h3>
       <Table />
     </>
